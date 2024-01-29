f1ba60299ee99e609c92bebd1e40d46b1e608023
Emine Shaban <emineshaban@icloud.com>
2024-01-29 12:02:56 +0200

    .

--- js/index.js
+++ js/index.js
@@ -1,24 +1,32 @@
-
 let a = document.getElementsByClassName("a")
 let q = document.getElementsByClassName("q")
 
 const buttonGroup = document.getElementsByClassName("button-group");
 
+
 const buttonGroupPressed = (e) => {
-    if (e.target.parentElement.parentElement.children[1].style.display == "block") {
-        console.log(e.target.parentElement.parentElement.children[1].className)
-        if (e.target.parentElement.parentElement.children[1].className == "a") {
-            console.log(e.target.className)
-            e.target.parentElement.parentElement.children[1].style.display = "none"
-           
+    console.log(e.target)
+    if (e.target.className == "q") {
+        console.log(e.target.parentElement.children[1].style.display)
+        if (e.target.parentElement.children[1].style.display == "" || e.target.parentElement.children[1].style.display == "none"){
+            e.target.parentElement.children[1].style.display = "block"
+            console.log(e.target.parentElement.children[1].style.display)
+        }else{
+            e.target.parentElement.children[1].style.display = "none"
+            console.log(e.target.parentElement.children[1].style.display)
+
         }
-    } else {
-        e.target.parentElement.parentElement.children[1].style.display = "block"
- 
+    }else if(e.target.className == "a"){
+        if(e.target.parentElement.children[1].style.display == "block"){
+            e.target.parentElement.children[1].style.display = "none"
+            console.log(e.target.parentElement.children[1].style.display)
+        }
     }
+
+    
 }
+ 
 
 for (let index = 0; index < buttonGroup.length; index++) {
     buttonGroup[index].addEventListener('click', buttonGroupPressed);
 }
-
